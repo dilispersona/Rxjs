a991662702c91aa6f963cf1f0ed2d47d4c04e81d
Dileep Cn <dileep.cn@valoremconsulting.com>
2016-11-17 10:24:59 +0530

    creating an observable stream of data and listening to it using an observer

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-Experiments with Rxjs5
-npm install webpack webpack-dev-server typescript typings ts-loader --save-dev
-node_modules\.bin\typings install dt~es6-shim --global --save 
-typescript compiler options goes in tsconfig.json
+Experiments with Rxjs5,
+
+npm install webpack webpack-dev-server typescript typings ts-loader --save-dev,
+
+node_modules\.bin\typings install dt~es6-shim --global --save,
+
+typescript compiler options goes in tsconfig.json,
+
 webpack options goes in webpack.config.json
--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
-alert("ok!!!");
+import { Observable } from 'rxjs';
+
+//create a observable stream of data
+let numbers = [1,2,34,45];
+let source = Observable.from(numbers);
+
+
+//observer maily contains 3 methods next, error, complete
+
+class MyObserver{
+    //Every time Observable has  a new item , it will invoke this method of observer
+    next(value){
+        console.log(`value: ${value}`);
+    }
+
+    // in case of error it will invoke error method of observer
+    error(e){
+        console.log(`error: ${e}`);
+    }
+
+    complete(){
+        console.log(`completed`);
+    }
+
+}
+
+//subscribe to listen to Observable stream of data
+//way to listen is by passing an Observer
+
+
+source.subscribe(new MyObserver());
