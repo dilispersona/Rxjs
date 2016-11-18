abd22c03a85b49df03f61940a33145d3e1194860
Dileep Cn <dileep.cn@valoremconsulting.com>
2016-11-18 11:01:03 +0530

    Using flatMap to Process inner Observables

--- main.ts
+++ main.ts
@@ -1,61 +1,38 @@
 import { Observable } from 'rxjs';
 
-//let circle = document.getElementById('circle');
-
 let output = document.getElementById('output');
 let button = document.getElementById('button');
-let click = Observable.fromEvent(button,'click');
-
-                    /*    .map((e: MouseEvent) => {
-                            return {
-                                x: e.clientX,
-                                y: e.clientY
-                            }
-                        }).delay(100);
-                        .filter(value => value.x < 500);*/
-
-                        
-/*function onNext (value) {    
-       circle.style.left = value.x + "px";
-       circle.style.top = value.y + "px"; 
-      console.log(value.x);
-}*/
+let click = Observable.fromEvent(button, 'click');
 
 
-/*
-source.subscribe(
-    ,
-    e => console.log(`error: ${e}`),
-    () =>  console.log(`completed`)
-);*/
+function load(url: string) {
 
+    return Observable.create(observer => {
 
-function load(url: string){
-    //instantiate a xmlhttprequest object
+        var xhr = new XMLHttpRequest();
 
-    var xhr =  new XMLHttpRequest();
-
-    // 
-
-    xhr.open("GET", url);// open the get request with the url 
-    xhr.send();// request will happen asyn , so we need another event handler to know when we get reponse 
-
-
-    //so we want to handle load event on xmlHttprequest  object.... this event would be raised when we get the response
-    
-    xhr.addEventListener("load", () => {
-        let movies = JSON.parse(xhr.responseText);
-        movies.forEach(m => {
-            let div = document.createElement("div");
-            div.innerText = m.title;
-            output.appendChild(div);
-        })
+        xhr.open("GET", url);
+        xhr.send();
+        xhr.addEventListener("load", () => {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
+        });
     })
 
 }
 
-click.subscribe(
-    e => load("movies.json"),
-    e => console.log(`error: ${e}`),
-    () =>  console.log(`completed`)
-);
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    })
+}
+
+click.flatMap(e => load("movies.json"))
+    .subscribe(
+       renderMovies,
+        e => console.log(`error: ${e}`),
+        () => console.log(`completed`)
+    );
